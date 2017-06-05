aa13f4d5d0c976adcb758f641812161455e5dd02
Shinya Yamamoto <SY@yamamotoshinya-no-MacBook-Pro.local>
2017-06-05 12:10:22 +0900

    set ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/chat';
+
+const COMMENTS: Comment[] = [
+  { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  public content = '';
+  public comments = COMMENTS;
 }
