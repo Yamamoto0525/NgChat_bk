cdd7d0eea88578544960088bf9e45ace9a1ba496
Shinya Yamamoto <SY@yamamotoshinya-no-MacBook-Pro.local>
2017-06-05 17:46:10 +0900

    set pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChatDatePipe } from './pipe/chat-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatDatePipe
   ],
   imports: [
     BrowserModule,
