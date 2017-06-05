03e171f44bf3abfefb31e7fa0058fee952235bba
Shinya Yamamoto <SY@yamamotoshinya-no-MacBook-Pro.local>
2017-06-05 15:35:49 +0900

    set ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { Comment } from './class/chat';
+import { Comment, User } from './class/chat';
 
-const COMMENTS: Comment[] = [
-  { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
-  { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
-  { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+const CURRENT_USER: User = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
+const ANOTHER_USER: User = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
+const COMMENTS: Comment[] = [ // クラスを元にコメントを作成
+	new Comment(ANOTHER_USER, 'Suzukiの１つ目のコメントです。'),
+	new Comment(ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
+	new Comment(CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
+	new Comment(ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
+	new Comment(CURRENT_USER, 'Tanakaの2つ目のコメントです。')
 ];
 
 @Component({
@@ -16,4 +20,14 @@ const COMMENTS: Comment[] = [
 export class AppComponent {
   public content = '';
   public comments = COMMENTS;
+  public current_user = CURRENT_USER; // 追加
+
+  // 新しいコメントを追加
+  addComment(comment: string) {
+     if (comment) {
+       this.comments.push(new Comment(this.current_user, comment));
+       this.content = '';
+     }
+  }
+
 }
